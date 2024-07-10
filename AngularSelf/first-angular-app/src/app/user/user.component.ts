eb2c9ec9ce6d8aee8d5e4b83f4345d787973b7c6
Swarn Singh <swarn.bedi@landisgyr.com>
2024-07-11 00:04:00 +0530

    Identification: Adding new code.
    Change-Id: Ie7d924900f385d06abdcff45aeb10641cb21cde9

--- AngularSelf/first-angular-app/src/app/user/user.component.ts
+++ AngularSelf/first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -10,6 +10,22 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+  selectedUser = signal(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+
+  imagePath = computed(() => '../../assets/users/' + this.selectedUser().avatar);
+
+  // When we use signals we dont do this way.
+ /*  get imagePath(){
+    return '../../assets/users/' + this.selectedUser.avatar;
+  } */
+
+  onUserClick(){
+    // (without signals)
+    //this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+
+    //with signals
+    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+    console.log("clicked !!!");
+  }
 
 }
