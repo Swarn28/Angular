a6a620d891259815011b6b41c1d92a12bf53de1d
Swarn28 <swarnbedi28@gmail.com>
2024-07-21 00:45:01 +0530

    Adding new component...

--- AngularSelf/first-angular-app/src/app/tasks/tasks.component.ts
+++ AngularSelf/first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,19 +1,34 @@
 import { DUMMY_USERS } from './../dummy-users';
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({required:true}) selectedId!: string;
+  @Input({required: true}) selectedId!: string;
+  @Input({required:true}) selectedName!: string;
 
-
-
-
-
-
+  tasks = [
+    { id: 't1',
+      userId: 'u1',
+      tittle: 'Master Angular',
+      summary: 'Learn Angular basics...',
+      dueDate : '2024-7-5'
+    },
+    { id: 't2',
+      userId: 'u3',
+      tittle: 'Play badminton',
+      summary: 'Playing is good',
+      dueDate : '2024-10-15'},
+    { id: 't3',
+      userId: 'u3',
+      tittle: 'Eat Healthy',
+      summary: 'Eating healthy food is good',
+      dueDate : '2024-8-6'}
+  ]
 }
