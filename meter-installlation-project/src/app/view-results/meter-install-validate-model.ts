ccd72ebaa3192a5316ac1a2c0992f5d96733a726
Swarn Singh <swarn.bedi@landisgyr.com>
2024-08-26 17:30:48 +0530

    Identification: Adding new code
    Change-Id: If0eb456dc5bca9035a03b9f265b34fffbfac4ea3

---
+++ meter-installlation-project/src/app/view-results/meter-install-validate-model.ts
@@ -0,0 +1,4 @@
+export interface MeterInstallValidateResponse{
+  id: string;
+  status: string;
+}
--- meter-installlation-project/src/app/view-results/view-results.component.ts
+++ meter-installlation-project/src/app/view-results/view-results.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { MeterInstallValidateResponse } from './meter-install-validate-model';
 
 @Component({
   selector: 'app-view-results',
@@ -11,10 +13,31 @@ export class ViewResultsComponent {
 
   @Input({required:true}) installedMetersList!: string[];
   @Output() closeButtonEvent = new EventEmitter<void>();
+  private httpClient = inject(HttpClient);
+  validatedInstalledMeters!: MeterInstallValidateResponse[];
 
 
   onCloseButton(){
     this.closeButtonEvent.emit();
+  }
+
+  onValidateButton(){
+    console.log("Starting validations...")
+    this.installedMetersList.push("IRN714945");
+    this.installedMetersList.push("IRN761557");
+    this.installedMetersList.push("IRN405233");
+
+
+    this.httpClient.post<MeterInstallValidateResponse[]>
+    ('http://localhost:8080/meterAction/v1/validateMeterInstall',this.installedMetersList).subscribe({
+      next: (response) => {
+        this.validatedInstalledMeters = response;
+        console.log("Fetched installed meters: " +this.validatedInstalledMeters);
+      },
+      error: (error) => {
+        console.log("Error while fetching validations is : "+error);
+      }
+    });
   }
 
 }
